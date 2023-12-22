152db53c7adb26f0fbcd9929baad0d0001980768
Hubert Mąka <hubertmaka@MacBook-Air-Hubert-2.local>
2023-12-23 00:12:54 +0100

    ICS-20 Add products site

--- shop-interface/src/App.jsx
+++ shop-interface/src/App.jsx
@@ -1,8 +1,11 @@
 import { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import './App.css';
+
 import Navbar from './components/Navbar';
 import Home from './components/sites/Home';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Products from './components/sites/Products';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +15,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
+          <Route path='/products' Component={Products} />
           <Route path='/' exact Component={Home}/>
         </Routes>
       </Router>
---
+++ shop-interface/src/components/sites/Products.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './Products.css';
+
+
+function Products() {
+    return (
+        <>
+            <div className="container">
+                <h1>Products</h1>
+            </div>
+        </>
+    );
+}
+
+export default Products;

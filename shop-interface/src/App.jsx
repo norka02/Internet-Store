e5076b9fec6197cadf36a4b16da7992c1e249a7b
Hubert Mąka <hubertmaka@MacBook-Air-Hubert-2.local>
2023-12-22 23:59:09 +0100

    ICS-18 Add Home page

--- shop-interface/src/App.jsx
+++ shop-interface/src/App.jsx
@@ -1,7 +1,8 @@
-import { useState } from 'react'
-import './App.css'
-import Navbar from './components/Navbar'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { useState } from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import Home from './components/sites/Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,12 +12,9 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/' exact />
+          <Route path='/' exact Component={Home}/>
         </Routes>
       </Router>
-      
-      
-     
     </>
   )
 }

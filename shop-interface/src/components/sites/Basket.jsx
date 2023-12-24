4976d10dacf17a290e7332e937c8d58520847001
Hubert Mąka <hubertmaka@MacBook-Air-Hubert-2.local>
2023-12-24 22:08:46 +0100

    ICS-21-Zakladka-koszyk

---
+++ shop-interface/src/components/sites/Basket.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Basket() {
+    return (
+        <>
+        <div className="container">
+           <h1>Basket</h1> 
+        </div>
+        </>
+    );
+}
+
+export default Basket;
